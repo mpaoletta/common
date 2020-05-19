540fbb30c33cdb9ad6725d42258f2dbcff2faa0d
Martin Paoletta <martin@redb.ee>
2020-05-19 00:00:33 -0300

    c4

--- src/main/scala/common/Common.scala
+++ src/main/scala/common/Common.scala
@@ -5,3 +5,5 @@ case class Common1(a: String, b: Int)
 case class Common2(a: String, b: Int, c: Double)
 
 case class Common3(a: String, b: Int, c: Double)
+
+case class Common4(a: String, b: Int, c: Double)
